e77b26b65c027b02ed20522eb03295a70f274df6
Caleb De La Cruz P <delacruzpaulino@gmail.com>
2017-09-14 04:02:38 -0700

    Temporary test of intersection instead of full jaccard

--- Algorithms/CB/CBAlgorithmJACCARD.py
+++ Algorithms/CB/CBAlgorithmJACCARD.py
@@ -17,7 +17,10 @@ from time import time
 from sklearn.feature_extraction.text import CountVectorizer
 from scipy.sparse import csr_matrix
 
+from Util import chunking_dot
 from .CBAlgorithm import CBAlgorithm
+import dask.array as da
+from chest import Chest
 
 logger = logging.getLogger(__name__)
 
@@ -33,15 +36,15 @@ class CBAlgorithmJACCARD(CBAlgorithm):
         :param data: Array of strings
         :return: Sparse matrix NxM where N is the same length of data and M is the number of features
         '''
-        super(CBAlgorithmJACCARD, self).index(data)
+        data = super(CBAlgorithmJACCARD, self).index(data)
 
         t0 = time()
         self.vectorizer = CountVectorizer(max_df=0.5, stop_words='english')
-        self.index = self.vectorizer.fit_transform(data)
+        self.indexed = self.vectorizer.fit_transform(data)
         duration = time() - t0
-        logger.info("n_samples: %d, n_features: %d" % self.index.shape)
-        logger.info("duration: %d\n" % duration)
-        return self.index
+        logger.info("n_samples: %d, n_features: %d" % self.indexed.shape)
+        logger.info("duration: %d" % duration)
+        return self.indexed
 
     def similarity(self, index=None):
         '''
@@ -50,7 +53,7 @@ class CBAlgorithmJACCARD(CBAlgorithm):
         :return: Sparse matrix NxN where every cell is the similarity of its indexes
         '''
         if index is None:
-            index = self.index
+            index = self.indexed
         super(CBAlgorithmJACCARD, self).similarity(index)
 
         t0 = time()
@@ -61,6 +64,8 @@ class CBAlgorithmJACCARD(CBAlgorithm):
         # Calculate intersection in a vectorized way
         intersection = binary_index.dot(binary_index.T)
 
+        return intersection
+
         # Calculate union in a vectorized way
         union = self.vectorized_union(intersection, binary_index)
 
@@ -70,7 +75,7 @@ class CBAlgorithmJACCARD(CBAlgorithm):
 
         duration = time() - t0
         logger.info("n_samples: %d, n_related_samples: %d" % result.shape)
-        logger.info("duration: %d\n" % duration)
+        logger.info("duration: %d" % duration)
 
         # TODO: Check the effect of using only the intersection score instead of the JACCARD score
         return  result
@@ -86,16 +91,27 @@ class CBAlgorithmJACCARD(CBAlgorithm):
         matrix_2 = np.append([ones.T], [row.T], axis=0).T
 
         # Step 3: Calculate the sum of each possible combination
+        matrix_1 = da.from_array(matrix_1, chunks=(1000))
+        matrix_2 = da.from_array(matrix_2, chunks=(1000))
+
         union = matrix_2.dot(matrix_1)
+        union = union.compute()
+        logger.info("Dot Product")
+
+        # cache = Chest(path='c:/temp', available_memory=13e9)
+        # union = matrix_2.dot(matrix_1)#.compute(cache=cache)
 
         # Step 4: Remove intersection
         # Step 4.1: 0 everything that does not have an intersection
         #           This step is not for UNION, but in JACCARD we dont care about
         #           Cells without intersection
-        sparse_union = csr_matrix((union[intersection.nonzero()], intersection.nonzero()), shape=intersection.shape)
 
+        indexes = intersection.nonzero()
+        logger.info("Data")
+        sparse_union = csr_matrix((union[indexes], indexes), shape=intersection.shape)
+        logger.info("Sparse")
         # Step 4.2: Remove Intersection
         sparse_union.data = sparse_union.data - intersection.data
-
+        logger.info(sparse_union.data[0:10])
         return sparse_union
 
