f03d27bd16506d0b3eda85c1e24f37f1fcee62e4
Julimer <raylu95.lr@gmail.com>
2017-09-21 09:52:56 -0700

    Add Userinfo relation

--- Web/mainsite/admin.py
+++ Web/mainsite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mainsite.models import Userinfo, Movie, Similarity
 
 # Register your models here.
+admin.site.register(Userinfo)
+admin.site.register(Movie)
+admin.site.register(Similarity)
+
+
+
