51b538433ac18f2fabf9ddf0d5cea4e1e86f5734
Caleb De La Cruz P <delacruzpaulino@gmail.com>
2017-09-25 16:47:21 -0700

    Add script to populate similarity tables

--- Algorithms/Util/__init__.py
+++ Algorithms/Util/__init__.py
@@ -1,23 +1,2 @@
-from enum import Enum
-
-
-class Field(Enum):
-    TITLE = 1
-    GENRE = 2
-    CAST = 3
-    PLOT = 4
-    FULL_PLOT = 5
-    AWARDS = 6
-
-    DIRECTOR = 7
-    WRITER = 8
-    LANGUAGE = 9
-    COUNTRY = 10
-
-    YEAR = 11
-    RATING = 12
-    RUNTIME = 13
-    RELEASED = 14
-    METACRITIC = 15
-    IMDB_RATING = 16
-    IMDB_VOTES = 17
+from enums import Field
+from populate_similarity_table import *
---
+++ Algorithms/Util/populate_similarity_table.py
@@ -0,0 +1,100 @@
+import sys
+import logging
+import numpy as np
+from DataHandler import PostgresDataHandler
+from enums import Field
+
+from CB import CBRecommender, CBAlgorithmTMDB, CBAlgorithmTFIDF, CBAlgorithmBM25, CBAlgorithmJACCARD, CBAlgorithmTMDB, CBAlgorithmTFIDF2
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+logger.handlers = []
+FORMAT = '%(message)s'
+formatter = logging.Formatter(fmt=FORMAT)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.propagate = False
+
+logger.warn('Start')
+
+# Load Data
+import pandas
+
+def main():
+    movies = pandas.read_csv(r'Data\ml-20m\movies.csv')
+    groundtruth = pandas.read_csv(r'Data\groundtruth.exp1.csv')
+
+    groundtruth.shape
+    groundtruth.dropna()
+
+    m1 = groundtruth['movieid1'].unique()
+    m2 = groundtruth['movieid2'].unique()
+    movies_ids_tagged = np.union1d(m1,m2)
+    print 'This are the tagged movies ids', len(movies_ids_tagged)
+
+    # Get the recommender
+    rec = CBRecommender()
+
+    # Get the connection with the 'database'
+    fields = [
+        Field.TITLE,\
+        Field.GENRE,\
+        Field.CAST,\
+        Field.PLOT,\
+        Field.FULL_PLOT,\
+        Field.AWARDS,\
+        Field.DIRECTOR,\
+        Field.WRITER,\
+        Field.LANGUAGE,\
+        Field.COUNTRY,\
+        # Field.YEAR,\
+        # Field.RATING,\
+        # Field.RUNTIME,\
+        # Field.RELEASED,\
+        # Field.METACRITIC,\
+        # Field.IMDB_RATING,\
+        # Field.IMDB_VOTES
+    ]
+
+    dataset = PostgresDataHandler()
+    dataset.clear_similarity()
+    for field in fields:
+        logger.warn('\n%s'% str(field))
+        data = dataset.get_data(field, movies_ids_tagged.tolist())
+        size = len(data)
+
+        # Single Field
+        logger.warn("%d records retrieved", len(data))
+        cache = {}
+        for algo in [CBAlgorithmJACCARD(), CBAlgorithmTFIDF(), CBAlgorithmBM25()]:
+            # Train the recommender using the given algorithm and dataset
+            result = rec.train(data, algo)
+
+            print 'Trained Data'
+
+            rows, cols = result.nonzero()
+
+            size = len(rows)
+
+            print 'Similarities', size
+            for i in range(size):
+                if i % 10000 == 0 and i > 0:
+                    print 'Percentage ', float(i)/size
+
+                row = rows[i]
+                col = cols[i]
+                id1 = algo.ids[row]
+                id2 = algo.ids[col]
+
+                score = result[row, col]
+                if id1 is None or id2 is None:
+                    print id1, id2
+                    raise Exception()
+
+                dataset.add_to_similarity_batch(id1, id2, field, algo.__name__, score)
+
+            # Release Memory
+            algo.destroy()
+
+    dataset.save_similarity_batch()
