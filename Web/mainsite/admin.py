e23697bf1f4979f450369684a0a106ffaa3537d6
Zhehui Zhou <grayy921013@gmail.com>
2017-10-05 14:50:21 -0700

    Change all id fields to integer

--- Web/mainsite/admin.py
+++ Web/mainsite/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from mainsite.models import Userinfo, Movie, Similarity
+from mainsite.models import Userinfo
 
 # Register your models here.
 admin.site.register(Userinfo)
-admin.site.register(Movie)
-admin.site.register(Similarity)
+# admin.site.register(Movie)
+# admin.site.register(Similarity)
 
 
 
