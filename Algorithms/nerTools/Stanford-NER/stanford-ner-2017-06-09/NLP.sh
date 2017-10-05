7cbbbbc690daa348ef4cb610599c759c60cf24d4
Momina <momina28haider@gmail.com>
2017-10-05 14:08:17 -0700

    Populate Filtered Plot

--- Algorithms/nerTools/Stanford-NER/stanford-ner-2017-06-09/NLP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILES=/Users/Momina/Documents/CMU_SV_Fall_2017/Practicuum/Stanford-NER/stanford-ner-2017-06-09/test_dir/*
-for f in $FILES
-do
-	java -mx600m  edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -outputFormat tabbedEntities -textFile "$f"  > "${f}_result.tsv"
-done
