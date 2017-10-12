1ed60c2dbda8241b562b2f29086604c88b74a925
Momina <momina28haider@gmail.com>
2017-10-12 02:20:55 -0700

    Fixed error with populate_filteredplot

--- Algorithms/Util/populate_filtered_plot.py
+++ Algorithms/Util/populate_filtered_plot.py
@@ -30,7 +30,7 @@ from mainsite.models import Movie,MovieFiltered_Plot
 
 
 def main(argv):
-    snert = StanfordNERTagger('nerTools/Stanford-NER/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz', 'nerTools/Stanford-NER/stanford-ner-2017-06-09/stanford-ner.jar')
+    snert = StanfordNERTagger('/nerTools/Stanford-NER/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz', '/nerTools/Stanford-NER/stanford-ner-2017-06-09/stanford-ner.jar')
     logging.basicConfig(level=logging.WARN)
     # Get the connection with the 'database'
     dataset = PostgresDataHandler()
@@ -39,13 +39,12 @@ def main(argv):
     matrix = np.array(data)
     ids = matrix[:, 0].astype(int)
     values = matrix[:, 1]
-    movie_index = 0
+    movie_index = 26000
     bulk = []
     bulk_count = 0
     t = time()
     print(t)
-    for value in values:
-        print ids[movie_index]
+    for value in values[-1000:]:
         tokens = nltk.word_tokenize(value.encode('utf-8').decode('utf-8'))
         tagged = snert.tag(tokens)
         #ne = nltk.ne_chunk(tagged_tokens=tagged,binary=True)
@@ -56,7 +55,7 @@ def main(argv):
         sentence = sentence.lstrip()
         bulk.append(MovieFiltered_Plot(id = ids[movie_index], filtered_plot=sentence))
         bulk_count = bulk_count + 1
-        if bulk_count >= 1000:
+        if bulk_count >= 1:
             print(time() - t)
             print("Saved Bulk")
             t = time()
@@ -67,8 +66,9 @@ def main(argv):
         #movie.filtered_plot = sentence
         #movie.save()
         movie_index = movie_index + 1
-    if bulk.size:
+    if bulk:
         MovieFiltered_Plot.objects.bulk_create(bulk)
+        print("Saved Last Bulk")
 
 
 if __name__ == "__main__":
