287ef42118415c29fa26b4fa6cf44cfd0c4c49bd
suanmiao <quinn.leng.666@gmail.com>
2017-10-18 19:57:19 -0400

    add animation for label page.

--- Web/static/js/label.js
+++ Web/static/js/label.js
@@ -43,9 +43,6 @@ $(document).ready(function () {
 
     var movie_list = [];
 
-    // Debug code.
-    // target_movie_id = '3';
-
     var bindClickListener = function (movie_id, action) {
         console.log("click " + action + " movie Id " + movie_id);
         $.post("/uservote",
@@ -58,17 +55,59 @@ $(document).ready(function () {
                 console.log("vote status " + status);
                 console.log(data);
 
-                var rest_list = []
                 for (var i = 0; i < movie_list.length; i++) {
                     if (movie_list[i].id == movie_id) {
-                        console.log("remove index " + i);
-                    } else {
-                        rest_list.push(movie_list[i])
+                        movie_list[i].status = action;
+                        refreshMovie(movie_id)
                     }
                 }
-                movie_list = rest_list;
-                bindMovie(movie_list)
             });
+    }
+
+    var refreshMovie = function (movie_id) {
+        for (var i = 0; i < ui_movie_list.length; i++) {
+            var uiHolder = ui_movie_list[i];
+            var uiTitle = $($(uiHolder).find(".similar-title")).find("b");
+            var uiPopoverData = $(uiHolder).find(".popoverData");
+            var uiImage = $(uiPopoverData).find("img");
+            var uiButtonHolder = $(uiHolder).find(".label-bar");
+            var uiButtonNo = $(uiButtonHolder).find(".btn-no");
+            var uiButtonSkip = $(uiButtonHolder).find(".btn-skip");
+            var uiButtonYes = $(uiButtonHolder).find(".btn-yes");
+
+            if (i < movie_list.length && movie_list[i].id == movie_id) {
+                var movie = movie_list[i];
+
+                // bind rating status
+                if (movie.status != 2) {
+                    $(uiHolder).addClass('animation-target')
+                    $(uiHolder).fadeTo(300, 0.5);
+
+                    switch (movie.status) {
+                        case -1:
+                            uiButtonNo.addClass('btn-glow');
+                            uiButtonSkip.removeClass('btn-glow');
+                            uiButtonYes.removeClass('btn-glow');
+                            break;
+                        case 0:
+                            uiButtonNo.removeClass('btn-glow');
+                            uiButtonSkip.addClass('btn-glow');
+                            uiButtonYes.removeClass('btn-glow');
+                            break;
+                        case 1:
+                            uiButtonNo.removeClass('btn-glow');
+                            uiButtonSkip.removeClass('btn-glow');
+                            uiButtonYes.addClass('btn-glow');
+                            break;
+                    }
+
+                } else {
+                    $(uiHolder).fadeTo(0, 1);
+                }
+
+            }
+        }
+
     }
 
     var bindMovie = function (movie_list) {
@@ -88,6 +127,8 @@ $(document).ready(function () {
 
                 var movie = movie_list[i];
 
+                // bind content
+
                 uiTitle.text(movie.title + " (" + movie.year + ")");
                 if (movie.plot) {
                     uiPopoverData.attr("data-content", movie.plot);
@@ -102,21 +143,50 @@ $(document).ready(function () {
 
                 $(uiButtonNo).unbind();
                 $(uiButtonNo).click(function () {
+                    $(this).blur();
                     var id = $(this).attr("id").substring(9);
                     bindClickListener(id, -1);
                 })
 
                 $(uiButtonSkip).unbind();
                 $(uiButtonSkip).click(function () {
+                    $(this).blur();
                     var id = $(this).attr("id").substring(9);
                     bindClickListener(id, 0);
                 })
 
                 $(uiButtonYes).unbind();
                 $(uiButtonYes).click(function () {
+                    $(this).blur();
                     var id = $(this).attr("id").substring(9);
                     bindClickListener(id, 1);
                 })
+
+                // bind rating status
+                if (movie.status != 2) {
+                    $(uiHolder).fadeTo(0, 0.5);
+
+                    switch (movie.status) {
+                        case -1:
+                            uiButtonNo.addClass('btn-glow');
+                            uiButtonSkip.removeClass('btn-glow');
+                            uiButtonYes.removeClass('btn-glow');
+                            break;
+                        case 0:
+                            uiButtonNo.removeClass('btn-glow');
+                            uiButtonSkip.addClass('btn-glow');
+                            uiButtonYes.removeClass('btn-glow');
+                            break;
+                        case 1:
+                            uiButtonNo.removeClass('btn-glow');
+                            uiButtonSkip.removeClass('btn-glow');
+                            uiButtonYes.addClass('btn-glow');
+                            break;
+                    }
+
+                } else {
+                    $(uiHolder).fadeTo(0, 1);
+                }
 
             } else {
                 // set the holder to be invisible
