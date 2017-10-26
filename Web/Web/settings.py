619b9face4b8a8ee1ed90707fec753dcdce7cc6f
Zhehui Zhou <grayy921013@gmail.com>
2017-10-26 13:34:07 -0700

    Calculate precision for each algorithm

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mainsite/../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- Web/mainsite/admin.py
+++ Web/mainsite/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from mainsite.models import Userinfo, Movie, Similarity, SimilarMovie, UserVote, SearchAction, VotedMovie, SearchStopword
-
+from django.conf.urls import url
+from django.shortcuts import render
+from django.db import connection
 
 class SearchActionAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'user')
@@ -17,6 +19,34 @@ class UserVoteAdmin(admin.ModelAdmin):
             return "Skip"
         else:
             return "Similar"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            url('analytics/', self.get_analytics),
+        ]
+        return my_urls + urls
+
+    def get_analytics(self, request):
+        context = dict()
+        not_similar_count = {}
+        precision = {}
+        with connection.cursor() as cursor:
+            cursor.execute("select count(CASE WHEN action=-1 THEN 1 END), algorithm from " +
+                           "mainsite_similarmovie,mainsite_uservote where movie1_id=movie_id and " +
+                           "movie2_id=similar_movie_id group by algorithm;")
+            not_similar = cursor.fetchall()
+            for tuple in not_similar:
+                not_similar_count[tuple[1]] = tuple[0]
+            cursor.execute("select count(CASE WHEN action=1 THEN 1 END), algorithm from " +
+                           "mainsite_similarmovie,mainsite_uservote where movie1_id=movie_id and " +
+                           "movie2_id=similar_movie_id group by algorithm;")
+            similar = cursor.fetchall()
+            for tuple in similar:
+                precision[tuple[1]] = tuple[0] / (tuple[0] + not_similar_count[tuple[1]])
+            context['precision'] = precision
+
+        return render(request, 'analytics.html', context)
 
 
 class VotedMovieAdmin(admin.ModelAdmin):
