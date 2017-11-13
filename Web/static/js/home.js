a97e598eeb105591ad491dccc7a918f5273710a2
julimer <raylu95.lr@gmail.com>
2017-11-13 14:18:55 -0800

    Add homepage step-by-step instruction

--- Web/static/js/home.js
+++ Web/static/js/home.js
@@ -1,18 +1,48 @@
 $(document).ready(function () {
     $(".popoverData").popover();
 
+    var current_img = 1;
+    var total_img = 3;
+
+    initGallery();
+
+    $('#show_prev').click(function () {
+        var hide_img_id = "img_" + current_img;
+        current_img--;
+        var show_img_id = "img_" + current_img;
+        $('#' + hide_img_id).hide();
+        $('#' + show_img_id).show();
+        updateGallery();
+    });
+
+    $('#show_next').click(function () {
+        if (current_img >= total_img) {
+            $('#helpModal').modal('hide');
+            initGallery();
+        }
+        else {
+            var hide_img_id = "img_" + current_img;
+            current_img++;
+            var show_img_id = "img_" + current_img;
+            $('#' + hide_img_id).hide();
+            $('#' + show_img_id).show();
+            updateGallery();
+        }
+    });
+
     // check if visited
     $.get("/check_visited?path=index", function (data) {
         if (data == "true") {
         } else if (data == "false") {
             // popup
+            initGallery();
             $('#helpModal').modal()
 
             // set that page as visited
             $.get("/visit_page?path=index")
         }
     })
-
+    
     var previousLabelCount = 0;
     // update goal
     setInterval(function () {
@@ -27,5 +57,26 @@ $(document).ready(function () {
             }
         })
     }, 1000);
+
+    function initGallery() {
+        current_img = 1;
+        $("#show_prev").hide();
+        $("#show_next").text("Next");
+        $('#img_1').show();
+        $('#img_2').hide();
+        $('#img_3').hide();
+    }
+
+    function updateGallery(selector) {
+        if (current_img >= total_img) {
+            $("#show_next").text("OK");
+        }
+        else if (current_img <= 1) {
+            $("#show_prev").hide();
+        }
+        else {
+            $("#show_prev").show();
+        }
+    }
 
 });
