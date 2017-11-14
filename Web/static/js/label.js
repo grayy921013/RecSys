a193af539d4daefb3fa186ab8b4a5f3723450898
julimer <raylu95.lr@gmail.com>
2017-11-13 16:47:05 -0800

    Add the label page instruction procedure

--- Web/static/js/label.js
+++ Web/static/js/label.js
@@ -1,11 +1,41 @@
 $(document).ready(function () {
     $(".popoverData").popover();
 
+    var current_img = 1;
+    var total_img = 4;
+
+    initGallery();
+
+    $('#show_prev').click(function () {
+        var hide_img_id = "img_" + current_img;
+        current_img--;
+        var show_img_id = "img_" + current_img;
+        $('#' + hide_img_id).hide();
+        $('#' + show_img_id).show();
+        updateGallery();
+    });
+
+    $('#show_next').click(function () {
+        if (current_img >= total_img) {
+            $('#helpModal').modal('hide');
+            initGallery();
+        }
+        else {
+            var hide_img_id = "img_" + current_img;
+            current_img++;
+            var show_img_id = "img_" + current_img;
+            $('#' + hide_img_id).hide();
+            $('#' + show_img_id).show();
+            updateGallery();
+        }
+    });
+
     // check if visited
     $.get("/check_visited?path=label", function (data) {
         if (data == "true") {
         } else if (data == "false") {
             // popup
+            initGallery();
             $('#helpModal').modal()
 
             // set that page as visited
@@ -27,6 +57,28 @@ $(document).ready(function () {
             }
         })
     }, 1000);
+
+    function initGallery() {
+        current_img = 1;
+        $("#show_prev").hide();
+        $("#show_next").text("Next");
+        $('#img_1').show();
+        $('#img_2').hide();
+        $('#img_3').hide();
+        $('#img_4').hide();
+    }
+
+    function updateGallery(selector) {
+        if (current_img >= total_img) {
+            $("#show_next").text("OK");
+        }
+        else if (current_img <= 1) {
+            $("#show_prev").hide();
+        }
+        else {
+            $("#show_prev").show();
+        }
+    }
 
 });
 
